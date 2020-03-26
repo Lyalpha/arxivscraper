aeaabf5d1a1da031d83e2ac0ce289858fb1508ea
jdl <joedlyman@gmail.com>
2020-03-26 16:37:16 +0000

    Remove unused code

--- arxivscraper/arxivscraper.py
+++ arxivscraper/arxivscraper.py
@@ -229,16 +229,6 @@ class Scraper(object):
         return ds
 
 
-def search_all(df, col, *words):
-    """
-    Return a sub-DataFrame of those rows whose Name column match all the words.
-    source: https://stackoverflow.com/a/22624079/3349443
-    """
-    return df[
-        np.logical_and.reduce([df[col].str.contains(word) for word in words])
-    ]
-
-
 cats = [
     "astro-ph",
     "cond-mat",
